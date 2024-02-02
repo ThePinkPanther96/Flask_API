a66901d0cb79dc8491a407554fff14334f3099b6
git <git@192.168.1.51>
2024-02-03 01:35:14 +0200

    updated app ip address

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='3.79.179.101', port=5000)
