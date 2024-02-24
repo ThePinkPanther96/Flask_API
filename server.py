f140d800096333d8f014fac1bf59539852de87c1
git <git@192.168.1.51>
2024-02-24 16:40:40 +0200

    app

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+def index():
+    return render_template('index.html')
